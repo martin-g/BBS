3c50ab6196a4aba1d4806ad50f34b01b86682564
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2023-05-16 13:06:37 +0300

    Add openEuler installation files
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

---
+++ openEuler-files/22.03/dnf_extra_fonts.sh
@@ -0,0 +1,11 @@
+# APT packages for extra fonts
+# gsfonts-x11
+# xfonts-base
+# xfonts-scalable
+# xfonts-100dpi
+# xfonts-75dpi
+# t1-xfree86-nonfree
+# ttf-xfree86-nonfree
+# ttf-xfree86-nonfree-syriac
+
+# dnf install -y ...
