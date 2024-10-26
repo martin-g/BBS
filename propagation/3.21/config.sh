389161293d25ad11f2b6ff4fc9a0161fc5723b79
J Wokaty <jwokaty@users.noreply.github.com>
2024-10-26 12:17:49 -0400

    Add 3.21 propagation

---
+++ propagation/3.21/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# =========================================================
+# Settings shared by all scripts in propagation/3.21 folder
+# =========================================================
+
+BIOC_VERSION="3.21"
+R_VERSION="4.5"
+Rscript="$HOME/bin/Rscript-$R_VERSION"
