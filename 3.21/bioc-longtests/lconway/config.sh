f56d6b8e19a76d9ae4f9abd1cff72691f8fb8f18
J Wokaty <jwokaty@users.noreply.github.com>
2024-11-22 14:45:35 -0500

    Add lconway to 3.21 workflows, longtests

--- 3.21/bioc-longtests/lconway/config.sh
+++ 3.21/bioc-longtests/lconway/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ====================
-# Settings for merida1
+# Settings for lconway
 # ====================
 
 
@@ -9,23 +9,19 @@
 
 export BBS_DEBUG="0"
 
-export BBS_NODE_HOSTNAME="merida1"
+export BBS_NODE_HOSTNAME="lconway"
 export BBS_USER="biocbuild"
-export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.21-workflows"
+export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.21-bioc-longtests"
 export BBS_R_HOME="/Library/Frameworks/R.framework/Resources"
 
-# merida1 has 24 logical CPUs.
-export BBS_NB_CPU=8
+# lconway has 48 logical CPUs.
+export BBS_NB_CPU=10
 
 # Central build node is bbscentral1 on Jetstream2.
 export BBS_CENTRAL_RHOST="bbscentral1"
 export BBS_RSH_CMD="ssh -F /Users/biocbuild/.ssh/config"
 export BBS_CENTRAL_ROOT_URL="http://149.165.171.124"
 export BBS_PRODUCT_TRANSMISSION_MODE="asynchronous"
-
-# Source tarballs produced during STAGE3 won't be propagated
-# so we don't need to push them to the central builder.
-export DONT_PUSH_SRCPKGS="1"
 
 
 
